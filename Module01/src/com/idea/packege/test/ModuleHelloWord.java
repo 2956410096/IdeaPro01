ed47558f4eb763fa10f1a8d0ed1079d9c11122d7
Jack <2956410096@qq.com>
2020-08-02 17:42:09 +0800

    Initial commit

---
+++ Module01/src/com/idea/packege/test/ModuleHelloWord.java
@@ -0,0 +1,7 @@
+package com.idea.packege.test;
+
+public class ModuleHelloWord {
+    public static void  main (String[] args){
+        System.out.println("hello word 111111122222.....");
+    }
+}
---
+++ src/com/idea/test/HelloWord.java
@@ -0,0 +1,8 @@
+package com.idea.test;
+
+public class HelloWord {
+
+    public static void  main (String[] args){
+        System.out.println("hello word 1111111.....");
+    }
+}
